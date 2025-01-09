5ecc945f1beaf0a724abdf90a1dfb6d6003e6942
tieu dieu <anhpt.l.1922@gmail.com>
2025-01-09 16:07:15 +0700

    popup

---
+++ Assets/UI/Popup/Popup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class Popup : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _text;
+    private Action _onYesAction;
+    public void SetText(string text)
+    {
+        _text.text = text;
+    }
+    public void SetYesAction(Action onYesAction)
+    {
+        _onYesAction = onYesAction;
+    }
+
+    // Gọi khi nút Yes được nhấn
+    public void YesButton()
+    {
+        _onYesAction?.Invoke(); // Thực thi hành động nếu không null
+        ClosePopup(); // Đóng popup sau khi nhấn
+    }
+    public void ClosePopup()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/UI/UIManagement.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManagement : MonoBehaviour
+{
+    [SerializeField] private List<Canvas> _screen;
+    [SerializeField] private Popup popup;
+
+ }
