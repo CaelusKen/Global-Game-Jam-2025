57a1d49902ce89fdab13e7c7e0309df6e3bbd009
tieu dieu <anhpt.l.1922@gmail.com>
2025-01-12 12:08:34 +0700

    switch camera

---
+++ Assets/Scripts/Camera/CameraSwitcher.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using Cinemachine;
+using DG.Tweening;
+using System.Collections.Generic;
+public class CameraSwitcher : MonoBehaviour
+{
+
+    [SerializeField] List<CinemachineVirtualCamera> cameras;
+    // Cinemachine Cameras
+    public int activeCamera = 0;
+
+    private void Reset()
+    {
+        cameras.Clear();  // Xóa dữ liệu cũ để tránh trùng lặp
+        cameras.AddRange(GetComponentsInChildren<CinemachineVirtualCamera>());
+    }
+    private void Start()
+    {
+        
+        cameras[0].Priority = 10;
+    }
+   
+    private void Update()
+    {
+      
+        // Phím 1 → Góc nhìn ngang (Orthographic)
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log("switch");
+            Switch();
+        }
+
+
+    }
+    private void HideCam()
+    {
+        foreach (var cam in cameras)
+        {
+            cam.Priority = 0;
+        }
+    }
+
+    
+
+    // Kích hoạt Cinemachine Camera
+    private void Switch()
+    {
+        HideCam();
+        activeCamera++;
+        if (activeCamera >= cameras.Count)
+        {
+            activeCamera = 0;
+        }
+        cameras[activeCamera].Priority = 10;
+    }
+}
